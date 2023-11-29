b7d18d64772cb13befa7426d659ccefd3113f3f4
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2023-11-29 17:42:44 -0500

    updated thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -53,7 +53,74 @@ const thoughtController = {
         })
         .catch((err) => res.json(err));
     },
+
+updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id}, body,{
+        new: true,
+        runValidators: true,
+    })
+    .then((thoughtData) => {
+if (!thoughtData) {
+    res.status(404).json({ message: "Error!"});
+    return;
+}
+res.json(thoughtData);
+    })
+    .catch((err) => res.json(err));   
+},
+
+deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id})
+    .then((thoughtData) => {
+        if (!thoughtData) {
+            return res.status(404).json({ message: "Error!"}); 
+        }
+        return User.findOneAndUpdate (
+            {thoughts: params.id },
+            {$pull: { thoughts: params.id }},
+            {new: true}
+        );
+    })
+    .then((dbUserData) => {
+        if (!dbUserData) {
+            return res
+            .status(404)
+            .json({ message: "Error! "});
     
+        }
+        res.json({message: "Success! "});
+
+    })
+    .catch((err) => res.json(err));
+},
+
+addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $addToSet: { reactions: body }},
+        { new: true, runValidators: true }
+    )
+    .then((thoughtData) => {
+        if (!thoughtData) {
+            res.status(404).json({ message: "Error! "});
+            return;
+        }
+        res.json(thoughtData);
+    })
+    .catch((err) => res.json(err));
+},
+removeReaction({params }, res) {
+    Thought.findOneAndUpdate(
+        { _id: params.thoughtId},
+        { $pull: { reactions: { reactionId:params.reactionId }}},
+        {new: true}
+    )
+    .then((thoughtData) => res.json(thoughtData))
+    .catch((err) => res.json(err));
+} ,
+};
+
+module.exports = thoughtController
 
 
 
