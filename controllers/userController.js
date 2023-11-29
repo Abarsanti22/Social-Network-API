82bfa36861a849e05567e0cfcc363c2e0da20a84
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2023-11-29 18:10:23 -0500

    updated user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -61,6 +61,51 @@ updateUser({ params, body }, res ) {
     })
     .catch((err) => res.json(err));
 },
+deleteUser({params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+    .then((dbUserData) => {
+        if (!dbUserData) {
+            return res.status(404).json({ message: "Error!"});
+        }
+        return Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
+      })
+      .then(() => {
+        res.json({ message: "User and associated thoughts deleted!" });
+      })
+      .catch((err) => res.json(err));
+  },
+  addFriend({ params }, res ) {
+    User.findOneAndUpdate(
+        { _id: params.userId},
+        {$addToSet: {friends: params.friendId}},
+        { new: true, runValidators: true }
+    )
+    .then((dbUserData) => {
+        if (!dbUserData) {
+            res.status(404).json ({ message: "Error!"});
+            return;
 
+        }
+        res.json(dbUserData);
 
-}
+    })
+    .catch((err) => res.json(err));
+  },
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+        { _id: params.userId},
+        { $pull: { friends: params.friendId }},
+        { new: true}
+    )
+    .then ((dbUserData) => {
+        if (!dbUserData) {
+            return res.status(404).json({ message: "Error!"});
+
+        }
+        res.json(dbUserData);
+    })
+    .catch((err) => res.json(err));
+},
+};
+module.exports = userController;
+
