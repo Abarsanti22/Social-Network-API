6c7e81ff6a95d63e952f8aeb8ecfb28928d0ff52
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2023-11-29 18:00:35 -0500

    added user controller

---
+++ controllers/userController.js
@@ -0,0 +1,66 @@
+const { User, Thought } = require("../models");
+
+const userController = {
+    getAllUser(req, res) {
+        User.find({})
+        .populate({
+            path: "friends",
+            select: "-__v",
+
+        })
+        .select("-__v")
+        .sort({ _id: -1 })
+        .then((dbUserData) => res.json(dbUserData))
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+    },
+    getUserById({ params}, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: "thoughts",
+            select: "-__v",
+        })
+        .populate({
+            path: "friends",
+            select: "-__v",
+        })
+        .select("-__v")
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                return res
+                .status(404)
+                .json({ message: "Error!"});
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+createUser ({ body}, res) {
+    User.create(body)
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => res.json(err));
+},
+
+updateUser({ params, body }, res ) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+        new: true,
+        runValidators: true,
+    })
+    .then((dbUserData) => {
+        if (!dbUserData) {
+            res.status(404).json({ message: "Error!"});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch((err) => res.json(err));
+},
+
+
+}
