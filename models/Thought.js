c19c42597e51e0e5834a911aff2e1ad5b90e2617
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2023-11-29 18:29:08 -0500

    updated thought js

--- models/Thought.js
+++ models/Thought.js
@@ -30,3 +30,40 @@ const ReactionSchema = new Schema(
     }
 );
 
+const ThoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: "Thought is Required",
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [ReactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+
+    }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+
+const Thought =model("Thought", ThoughtSchema);
+module.exports = Thought;
+
